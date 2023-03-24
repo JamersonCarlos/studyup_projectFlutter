f6196267a345c9b78483d66ab3f6091c47562ee7
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-03-24 09:58:16 -0300

    validation of schedules

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       title: 'Study Up',
       home: BlocProvider(
         create: (context) => CalendarCubit(),
-        child: const FormTime(),
+        child: const AutheticationPage(),
       ),
       theme: ThemeData(primarySwatch: Colors.indigo, useMaterial3: true),
     );
--- lib/pages/PageFreeTime.dart
+++ lib/pages/PageFreeTime.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:day_night_time_picker/day_night_time_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,7 +8,9 @@ import 'package:intl/intl.dart';
 import 'package:time_picker_spinner_pop_up/time_picker_spinner_pop_up.dart';
 
 class FormTime extends StatefulWidget {
-  const FormTime({super.key});
+  const FormTime({super.key, required this.uid});
+
+  final String uid;
 
   @override
   State<FormTime> createState() => _FormTimeState();
@@ -15,6 +18,7 @@ class FormTime extends StatefulWidget {
 
 class _FormTimeState extends State<FormTime> {
   Time _time = Time(hour: 11, minute: 30, second: 20);
+
   final List<String> days_week = [
     "Segunda-feira",
     "Terça-feira",
@@ -43,15 +47,23 @@ class _FormTimeState extends State<FormTime> {
     [
       {"start": "00:00", "end": "00:00"}
     ],
-    [
-      {"start": "00:00", "end": "00:00"}
-    ]
   ];
 
   void onTimeChanged(Time newTime) {
     setState(() {
       _time = newTime;
     });
+  }
+
+  bool TimeValid(String start, String end) {
+    var listTimeStart = start.split(":");
+    var listTimeEnd = end.split(":");
+    bool validTime =
+        (int.parse(listTimeEnd[0]) - int.parse(listTimeStart[0])) > 0
+            ? true
+            : false;
+    print(validTime);
+    return validTime;
   }
 
   @override
@@ -70,112 +82,124 @@ class _FormTimeState extends State<FormTime> {
               padding: EdgeInsets.all(8),
               itemCount: days_week.length,
               itemBuilder: (BuildContext context, index) {
-                return Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      children: [
-                        Text(
-                          days_week[index],
-                          style: GoogleFonts.roboto(
-                              color: Colors.white,
-                              fontSize: 24,
-                              fontWeight: FontWeight.bold),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Text(
-                              "Das  ",
-                              style: GoogleFonts.roboto(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            GestureDetector(
-                              child: Container(
-                                height: 25,
-                                width: 70,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(2),
-                                  color: Colors.white,
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(2.0),
-                                  child: Row(
-                                    children: [
-                                      const Icon(
-                                        Icons.timer_outlined,
-                                        color: Colors.black,
-                                      ),
-                                      Text(free_times[index][0]["start"])
-                                    ],
-                                  ),
-                                ),
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: TimeValid(free_times[index][0]["start"],
+                              free_times[index][0]["end"])
+                          ? Colors.blueAccent
+                          : Colors.red,
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            days_week[index],
+                            style: GoogleFonts.roboto(
+                                color: Colors.white,
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Das  ",
+                                style: GoogleFonts.roboto(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
                               ),
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  showPicker(
-                                    context: context,
-                                    value: _time,
-                                    onChange: onTimeChanged,
-                                    onChangeDateTime: (DateTime dateTime) {
-                                      setState(() {
-                                        free_times[index][0]["start"] =
-                                            DateFormat("H:m").format(dateTime);
-                                      });
-                                    },
+                              GestureDetector(
+                                child: Container(
+                                  height: 25,
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(2),
+                                    color: Colors.white,
                                   ),
-                                );
-                              },
-                            ),
-                            Text(
-                              "   as   ",
-                              style: GoogleFonts.roboto(
-                                  color: Colors.white,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            GestureDetector(
-                              child: Container(
-                                height: 25,
-                                width: 70,
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(2),
-                                  color: Colors.white,
-                                ),
-                                child: Padding(
-                                  padding: const EdgeInsets.all(2.0),
-                                  child: Row(
-                                    children: [
-                                      const Icon(
-                                        Icons.timer_outlined,
-                                        color: Colors.black,
-                                      ),
-                                      Text(free_times[index][0]["end"])
-                                    ],
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(2.0),
+                                    child: Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.timer_outlined,
+                                          color: Colors.black,
+                                        ),
+                                        Text(free_times[index][0]["start"])
+                                      ],
+                                    ),
                                   ),
                                 ),
+                                onTap: () {
+                                  Navigator.of(context).push(
+                                    showPicker(
+                                      context: context,
+                                      value: _time,
+                                      onChange: onTimeChanged,
+                                      onChangeDateTime: (DateTime dateTime) {
+                                        setState(() {
+                                          free_times[index][0]["start"] =
+                                              DateFormat("H:m")
+                                                  .format(dateTime);
+                                        });
+                                      },
+                                    ),
+                                  );
+                                },
                               ),
-                              onTap: () {
-                                Navigator.of(context).push(
-                                  showPicker(
-                                    context: context,
-                                    value: _time,
-                                    onChange: onTimeChanged,
-                                    onChangeDateTime: (DateTime dateTime) {
-                                      setState(() {
-                                        free_times[index][0]["end"] =
-                                            DateFormat("H:m").format(dateTime);
-                                      });
-                                    },
+                              Text(
+                                "   as   ",
+                                style: GoogleFonts.roboto(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              GestureDetector(
+                                child: Container(
+                                  height: 25,
+                                  width: 70,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(2),
+                                    color: Colors.white,
                                   ),
-                                );
-                              },
-                            ),
-                          ],
-                        ),
-                      ],
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(2.0),
+                                    child: Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.timer_outlined,
+                                          color: Colors.black,
+                                        ),
+                                        Text(free_times[index][0]["end"])
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                onTap: () {
+                                  Navigator.of(context).push(
+                                    showPicker(
+                                      context: context,
+                                      value: _time,
+                                      onChange: onTimeChanged,
+                                      onChangeDateTime: (DateTime dateTime) {
+                                        setState(() {
+                                          free_times[index][0]["end"] =
+                                              DateFormat("H:m")
+                                                  .format(dateTime);
+                                        });
+                                      },
+                                    ),
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 );
@@ -183,7 +207,7 @@ class _FormTimeState extends State<FormTime> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.only(bottom: 20),
+            padding: const EdgeInsets.all(20),
             child: TextButton(
               style: TextButton.styleFrom(
                 backgroundColor: Colors.green,
@@ -191,9 +215,35 @@ class _FormTimeState extends State<FormTime> {
                   borderRadius: BorderRadius.circular(2),
                 ),
               ),
-              onPressed: () {},
+              onPressed: () {
+                List<dynamic> listInvalid = [];
+                for (int i = 0; i < free_times.length; i++) {
+                  if (TimeValid(
+                      free_times[i][0]["start"], free_times[i][0]["end"])) {
+                  } else {
+                    print(i);
+                    print(free_times[i][0]);
+                    listInvalid.add(free_times[i][0]);
+                  }
+                }
+                if (listInvalid.isNotEmpty) {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(
+                      backgroundColor: Colors.redAccent,
+                      duration: Duration(seconds: 3),
+                      content: Text(
+                        "Horários em vermelho estão inválidos!",
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  );
+                } else {
+                  print("formulário tá show");
+                }
+              },
               child: Text(
-                "Enviar",
+                "    Enviar    ",
                 style: GoogleFonts.roboto(
                     color: Colors.white, fontWeight: FontWeight.bold),
               ),
@@ -201,6 +251,43 @@ class _FormTimeState extends State<FormTime> {
           )
         ],
       ),
+    );
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Colors.redAccent,
+          elevation: 10,
+          title: Text(
+            'Horários Inválidos',
+            style: GoogleFonts.roboto(
+                color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text(
+                  'Os horários em vermelho estão inválidos, corriga e tente enviar novamente!',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ],
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text(
+                'OK',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
