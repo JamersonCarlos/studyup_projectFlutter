cac3337c9bf06f1ba4f5faa651e51db3b8eff29a
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-03-29 23:29:15 -0300

    adjusting annotation template

--- lib/models/anotations.dart
+++ lib/models/anotations.dart
@@ -2,58 +2,69 @@
 import 'dart:convert';
 
 class Anotation {
-   final DateTime envio;
-   final String description;
-   final List<String> videos;
-   final List<String> imagens;
-   final List<String> audios;
+  final String envio;
+  final String title;
+  final String description;
+  final List<String> videos;
+  final List<String> imagens;
+  final List<String> audios;
+  final List<String> links;
   Anotation({
     required this.envio,
+    required this.title,
     required this.description,
     required this.videos,
     required this.imagens,
     required this.audios,
+    required this.links,
   });
 
-  Anotation copyWith({
-     DateTime? envio,
-     String? description,
-     List<String>? videos,
-     List<String>? imagens,
-     List<String>? audios,
-  }) {
+  Anotation copyWith(
+      {String? envio,
+      String? title,
+      String? description,
+      List<String>? videos,
+      List<String>? imagens,
+      List<String>? audios,
+      List<String>? links}) {
     return Anotation(
       envio: envio ?? this.envio,
+      title: title ?? this.title,
       description: description ?? this.description,
       videos: videos ?? this.videos,
       imagens: imagens ?? this.imagens,
       audios: audios ?? this.audios,
+      links: links ?? this.links,
     );
   }
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'envio': envio,
+      'title': title,
       'description': description,
       'videos': videos,
       'imagens': imagens,
       'audios': audios,
+      'links': links
     };
   }
 
   factory Anotation.fromMap(Map<String, dynamic> map) {
     return Anotation(
-      envio: map['envio'] as DateTime,
-      description:map['description'] as String,
-      videos:  map['videos'] as List<String>,
-      imagens: map['imagens'] as List<String>,
-      audios: map['audios'] as List<String>,
-    );
+        envio: map['envio'] as String,
+        title: map['title'] as String,
+        description: map['description'] as String,
+        videos: map['videos'] as List<String>,
+        imagens: map['imagens'] as List<String>,
+        audios: map['audios'] as List<String>,
+        links: map['links'] as List<String>);
   }
 
   String toJson() => json.encode(toMap());
 
-  factory Anotation.fromJson(String source) => Anotation.fromMap(json.decode(source) as Map<String, dynamic>);
+  factory Anotation.fromJson(String source) =>
+      Anotation.fromMap(json.decode(source) as Map<String, dynamic>);
 
   @override
   String toString() {
@@ -63,21 +74,20 @@ class Anotation {
   @override
   bool operator ==(covariant Anotation other) {
     if (identical(this, other)) return true;
-  
-    return 
-      other.envio == envio &&
-      other.description == description &&
-      other.videos == videos &&
-      other.imagens == imagens &&
-      other.audios == audios;
+
+    return other.envio == envio &&
+        other.description == description &&
+        other.videos == videos &&
+        other.imagens == imagens &&
+        other.audios == audios;
   }
 
   @override
   int get hashCode {
     return envio.hashCode ^
-      description.hashCode ^
-      videos.hashCode ^
-      imagens.hashCode ^
-      audios.hashCode;
+        description.hashCode ^
+        videos.hashCode ^
+        imagens.hashCode ^
+        audios.hashCode;
   }
 }
