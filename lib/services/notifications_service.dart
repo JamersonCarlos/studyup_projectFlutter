279d1a51284635c03975b00b59ce310ac9ebf0dc
hellyaxs <eliasvitor.dev@gmail.com>
2023-04-25 08:53:19 -0300

    notificação teste

--- lib/services/notifications_service.dart
+++ lib/services/notifications_service.dart
@@ -78,7 +78,7 @@ class NotificationsService {
         priority: Priority.max,
         enableVibration: true);
 
-    DateTime date = DateTime.now().add(const Duration(seconds: 3));
+    DateTime date = DateTime.now().add(const Duration(seconds: 300));
     localNotificationsPlugin.zonedSchedule(
         notification.id,
         notification.title,
